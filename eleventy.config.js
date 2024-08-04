89628686ab68a01a0b24cb4585465cd3b8251e67
vickytnz <vicky.teinaki@gmail.com>
2024-08-04 17:12:23 +0100

    minor tweak

--- eleventy.config.js
+++ eleventy.config.js
@@ -40,7 +40,7 @@ module.exports = function (eleventyConfig) {
   })
 
   // Passthrough
-  eleventyConfig.addPassthroughCopy({ '/app/images': '.' })
+  eleventyConfig.addPassthroughCopy({ './app/images': '.' })
 
   // Config
   return {
